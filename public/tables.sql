2217fa100e5482ba514e40e0587a9ee2745e89fc
tamaron1820 <tatsuhiko02182002@gmail.com>
2023-06-04 13:54:39 -0700

    edit linter

--- public/tables.sql
+++ public/tables.sql
@@ -8,3 +8,23 @@ CREATE TABLE "roomtypes" (
 	"number"	INTEGER NOT NULL,
 	PRIMARY KEY("roomtype")
 );
+CREATE TABLE "sqlite_sequence" (
+	"name"	,
+	"seq"
+);
+CREATE TABLE "reviews" (
+	"id"	INTEGER,
+	"username"	INTEGER NOT NULL,
+	"rating"	INTEGER NOT NULL,
+	"title"	TEXT NOT NULL,
+	"comment"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE "bookings" (
+	"id"	INTEGER,
+	"username"	BLOB NOT NULL,
+	"roomtype"	REAL NOT NULL,
+	"date"	TEXT NOT NULL,
+	"confirmation_number"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
